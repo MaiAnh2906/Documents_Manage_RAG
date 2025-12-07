beb846f03212db3bd1f3928c24c0703e5de5f54a
MaiAnh2906 <phamthimaianh29@gmail.com>
2025-12-07 23:05:25 +0700

    manh

--- public/index.php
+++ public/index.php
@@ -245,12 +245,14 @@ $role = $_SESSION['login']['role'];
                     </button>
                 </li>
 
+                <?php if ($_SESSION['login']['role'] == 1) { ?>
                 <li
                     class="flex-center cursor-pointer p-16-semibold w-full whitespace-nowrap">
                     <button class="p-16-semibold flex size-full gap-4 p-2 group font-semibold rounded-lg hover:bg-blue-100 hover:shadow-inner focus:bg-[#2c70ceff] focus:text-white text-gray-700 transition-all ease-linear">
                         <a href="#" class="nav-link"><i class="bi bi-clock-history"></i> Recents</a>
                     </button>
                 </li>
+                <?php } ?>
 
                 <?php if ($_SESSION['login']['role'] == 1) { ?>
                     <li
