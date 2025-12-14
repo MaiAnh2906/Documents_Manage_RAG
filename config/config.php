6fa33e441a30e937983e1b19c5f0675f833a714a
MaiAnh2906 <phamthimaianh29@gmail.com>
2025-12-14 23:11:10 +0700

    manh

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = '';
-$DB_NAME = 'driver_cnw'; // ten csdl
+$DB_NAME = 'k73_nhom6'; // ten csdl
 $conn = mysqli_connect(hostname: $DB_HOST, username: $DB_USER, password: $DB_PASS, database: $DB_NAME);
 
 ?>
